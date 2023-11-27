e87c0fcb1964fdefa5bf4f92a79e04444d6f137d
TomKeny <tomkenyon2003@gmail.com>
2023-11-27 15:48:26 +0000

    added animation of color to nav links

--- src/NavLink.js
+++ src/NavLink.js
@@ -1,19 +1,53 @@
+import { motion } from "framer-motion"
+import { useState } from "react"
+
 export function NavLink ({week, setWeek, SetDisplaying}) {
+
+    let titles = ["Home","Week 1","Week 2","Week 3","Week 4","Week 5","Week 6","Week 7","Week 8","Week 9","Week 10","Week 11"]
+    const [hover,setHover] = useState([false,false,false,false,false,false,false,false,false,false,false,false])
 
     return (
         <>
-            <li><a onClick={() => {setWeek(0); SetDisplaying([false,false,false,false,false])}} id={week == 0 ? "active":"inactive"}>Home</a></li>
-            <li><a onClick={() => {setWeek(1); SetDisplaying([false,false,false,false,false])}} id={week == 1 ? "active":"inactive"}>Week 1</a></li>
-            <li><a onClick={() => {setWeek(2); SetDisplaying([false,false,false,false,false])}} id={week == 2 ? "active":"inactive"}>Week 2</a></li>
-            <li><a onClick={() => {setWeek(3); SetDisplaying([false,false,false,false,false])}} id={week == 3 ? "active":"inactive"}>Week 3</a></li>
-            <li><a onClick={() => {setWeek(4); SetDisplaying([false,false,false,false,false])}} id={week == 4 ? "active":"inactive"}>Week 4</a></li>
-            <li><a onClick={() => {setWeek(5); SetDisplaying([false,false,false,false,false])}} id={week == 5 ? "active":"inactive"}>Week 5</a></li>
-            <li><a onClick={() => {setWeek(6); SetDisplaying([false,false,false,false,false])}} id={week == 6 ? "active":"inactive"}>Week 6</a></li>
-            <li><a onClick={() => {setWeek(7); SetDisplaying([false,false,false,false,false])}} id={week == 7 ? "active":"inactive"}>Week 7</a></li>
-            <li><a onClick={() => {setWeek(8); SetDisplaying([false,false,false,false,false])}} id={week == 8 ? "active":"inactive"}>Week 8</a></li>
-            <li><a onClick={() => {setWeek(9); SetDisplaying([false,false,false,false,false])}} id={week == 9 ? "active":"inactive"}>Week 9</a></li>
-            <li><a onClick={() => {setWeek(10); SetDisplaying([false,false,false,false,false])}} id={week == 10 ? "active":"inactive"}>Week 10</a></li>
-            <li><a onClick={() => {setWeek(11); SetDisplaying([false,false,false,false,false])}} id={week == 11 ? "active":"inactive"}>Week 11</a></li>
+            {titles.map(function (title,index) {
+                return oneLink(title,index,week, setWeek, SetDisplaying, hover, setHover)
+            }) }
         </>
     )
+}
+
+function oneLink (title,index,week, setWeek, SetDisplaying,hover,setHover) {
+    return <li><motion.a 
+    onClick={() => {setWeek(index); SetDisplaying([false,false,false,false,false])}} 
+    key={index + title[index]}
+    onMouseOver={() => setTrue(index,hover,setHover)}
+    onMouseLeave={() => setFalse(index,hover,setHover)}
+    animate={color(week,hover,index)}>
+        {title}
+        </motion.a></li>
+}
+
+function setTrue(index,hover,setHover) {
+    let temparr = [...hover]
+    temparr[index] = true
+    setHover(temparr)
+    //console.log("hovering")
+}
+
+function setFalse(index,hover,setHover) {
+    let temparr = [...hover]
+    temparr[index] = false
+    setHover(temparr)
+    //console.log("not hovering")
+}
+
+function color (week, hover, index) {
+    if (hover[index]) {
+        return {backgroundColor: "rgb(51, 182, 110)"}
+    }
+    else if (week == index) {
+        return {backgroundColor: "rgb(109, 243, 149)"}
+    }
+    else {
+        return {backgroundColor: "rgb(95, 189, 122)"}
+    }
 }
